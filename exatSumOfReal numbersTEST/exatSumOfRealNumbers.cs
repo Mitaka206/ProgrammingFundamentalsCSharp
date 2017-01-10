f8c8aca3d3e4a65456b5e08c3276cb88d1a9e4bc
Mitaka206 <mitaka206@gmail.com>
2017-01-10 09:10:19 +0200

    test folder

---
+++ exatSumOfReal numbersTEST/exatSumOfRealNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exatSumOfReal_numbers
+{
+    class exatSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal ok = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+               decimal num = decimal.Parse(Console.ReadLine());
+                ok += num;
+            }
+
+            Console.WriteLine(ok);
+        }
+    }
+}
