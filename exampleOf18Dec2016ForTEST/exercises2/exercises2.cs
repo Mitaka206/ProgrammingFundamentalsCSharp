955c5b8ffedd4da5aa076689d897b94d8e5a9685
Mitaka206 <mitaka206@gmail.com>
2017-01-10 09:18:25 +0200

    add exam for test

---
+++ exampleOf18Dec2016ForTEST/exercises2/exercises2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exercises2
+{
+    public class exercises2
+    {
+        public static void Main(string[] args)
+        {
+
+            double safeMoney = double.Parse(Console.ReadLine());
+            double flourWidth = double.Parse(Console.ReadLine());
+            double flourLong = double.Parse(Console.ReadLine());
+            double aTriangle = double.Parse(Console.ReadLine());
+            double hTriangle = double.Parse(Console.ReadLine());
+            double priceOfPlate= double.Parse(Console.ReadLine());
+            double masterPrice = double.Parse(Console.ReadLine());
+
+            double flourArea = flourLong * flourWidth;
+            double triangleArea = (aTriangle * hTriangle) / 2;
+
+            double necesaryPlate = Math.Ceiling(flourArea / triangleArea) + 5;
+
+            double sumPrice = necesaryPlate * priceOfPlate + masterPrice;
+
+            if (safeMoney>=sumPrice)
+            {
+                double differens = safeMoney - sumPrice;
+                Console.WriteLine("{0:f2} lv left. ", differens);
+            }
+            else
+            {
+                double differens = sumPrice - safeMoney;
+                Console.WriteLine("You'll need");
+                Console.WriteLine("{0:f2} lv more.", differens);
+            }
+
+        }
+    }
+}
---
+++ exampleOf18Dec2016ForTEST/exercises3/exercises3.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace exercises3
+{
+    public class exercises3
+    {
+        public static void Main(string[] args)
+        {
+
+            double chrizantems = double.Parse(Console.ReadLine());
+            double rouses = double.Parse(Console.ReadLine());
+            double tulips = double.Parse(Console.ReadLine());
+
+            string season = Console.ReadLine().ToLower();
+            string holidayOrNot = Console.ReadLine().ToLower();
+
+            double bonusSevenUp = 0.95;//5% spring
+            double bonusTenUp = 0.9;//10% winter
+            double bonusTwentyUp = 0.8;//20% all seasons
+
+            double holidayIncreast = 1.15;//15% for holidays
+
+            double allFlower = chrizantems + rouses + tulips;
+            double priceAllFlowers = 0;
+            
+
+            switch (season)
+            {
+                case "spring":
+                    priceAllFlowers = 2 * chrizantems + 4.1 * rouses + 2.5 * tulips;
+                    switch (holidayOrNot)
+                    {
+                        case "y":
+                            priceAllFlowers *= holidayIncreast;
+                            break;
+                        case "n":
+                            priceAllFlowers *= 1;
+                            break;
+                    }
+                    if (tulips>=7)
+                    {
+                        priceAllFlowers *= bonusSevenUp;
+                    }
+                    
+                    break;
+                case "summer":
+                    priceAllFlowers = 2 * chrizantems + 4.1 * rouses + 2.5 * tulips;
+                    switch (holidayOrNot)
+                    {
+                        case "y":
+                            priceAllFlowers *= holidayIncreast;
+                            break;
+                        case "n":
+                            priceAllFlowers *= 1;
+                            break;
+                    }
+                    break;
+                case "autumn":
+                    priceAllFlowers = 3.75 * chrizantems + 4.5 * rouses + 4.15 * tulips;
+                    switch (holidayOrNot)
+                    {
+                        case "y":
+                            priceAllFlowers *= holidayIncreast;
+                            break;
+                        case "n":
+                            priceAllFlowers *= 1;
+                            break;
+                    }
+                    break;
+                case "winter":
+                    priceAllFlowers = 3.75 * chrizantems + 4.5 * rouses + 4.15 * tulips;
+                    switch (holidayOrNot)
+                    {
+                        case "y":
+                            priceAllFlowers *= holidayIncreast;
+                            break;
+                        case "n":
+                            priceAllFlowers *= 1;
+                            break;
+                    }
+                    if (rouses >= 10)
+                    {
+                        priceAllFlowers *= bonusTenUp;
+                    }
+                    break;
+            }
+            
+            if (allFlower>=20)
+            {
+                priceAllFlowers *= bonusTwentyUp;
+            }
+            priceAllFlowers += 2;
+            Console.WriteLine("{0:f2}", priceAllFlowers);
+
+
+        }
+    }
+}
