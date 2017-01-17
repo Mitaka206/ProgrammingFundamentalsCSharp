60a291dbb778f03e85f59446b94018fd06c5b99c
Mitaka206 <mitaka206@gmail.com>
2017-01-17 12:17:28 +0200

    add first zad of Exam of 23 October 2016/p01_CharityMarathon/p01_CharityMarathon.cs

--- Exams/Exam of 23 October 2016/p02_Ladybugs/p02_Ladybugs.cs
+++ Exams/Exam of 23 October 2016/p02_Ladybugs/p02_Ladybugs.cs
@@ -8,6 +8,14 @@ class p02_Ladybugs
 {
     static void Main(string[] args)
     {
+        int rows = int.Parse(Console.ReadLine());
 
+        string word = "";
+        while (word.Equals("stop"))
+        {
+
+
+            word = Console.ReadLine();
+        }
     }
 }
