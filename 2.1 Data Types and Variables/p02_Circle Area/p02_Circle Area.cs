10c59148381b1effd3c86e972aeb56da69969968
Mitaka206 <mitaka206@gmail.com>
2017-01-18 20:27:47 +0200

    sda

---
+++ 2.1 Data Types and Variables/p02_Circle Area/p02_Circle Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02_Circle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
