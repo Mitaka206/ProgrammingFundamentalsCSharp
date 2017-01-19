4ead5f83c064733441028778118455c0389cf901
Mitaka206 <mitaka206@gmail.com>
2017-01-19 14:24:31 +0200

    add other Files

---
+++ 3.1 MethodsDebuggingAndTroubleshootingCode/p00_Demo/p00_demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p00_Demo
+{
+    class p00_demo
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 3.1 MethodsDebuggingAndTroubleshootingCode/p01_BlankReceipt/p01_BlankReceipt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01_BlankReceipt
+{
+    class p01_BlankReceipt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nekakav text");
+        }
+    }
+}
