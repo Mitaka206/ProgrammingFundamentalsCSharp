2de404dfeb6087eb9f1e97f39ff0ad87fdab5694
Mitaka206 <mitaka206@gmail.com>
2017-01-20 10:11:43 +0200

    add round numbers

---
+++ 4.1 Arrays/p05_RoundNumbersFromZero/p05_RoundNumbersFromZero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p05_RoundNumbersFromZero
+{
+    class p05_RoundNumbersFromZero
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            decimal[] numbers = new decimal[input.Length];
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = decimal.Parse(input[i]);
+
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i])}");
+            }
+            //whay don't working
+        }
+    }
+}
