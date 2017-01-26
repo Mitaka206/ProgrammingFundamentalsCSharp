bdee7bce3278207180a9da46321c5fc7c8fcab86
Mitaka206 <mitaka206@gmail.com>
2017-01-26 16:20:52 +0200

    add arr exercises folder

---
+++ 4.2 Arrays - Exercise/p00_Demo/p00_Demo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace p00_Demo
+{
+    class p00_Demo
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //----------Largest Common End---------------
+
+            //string[] firstTex = Console.ReadLine().Split();
+            //string[] secondText = Console.ReadLine().Split();
+
+            //int[] maxArray = new int[Math.Min(firstTex.Length, secondText.Length)];
+
+            //int countLeft = 0;
+            //int countRight = 0;
+
+            //for (int i = 0; i < maxArray.Length; i++)
+            //{
+
+            //    if (firstTex[i] == secondText[i])
+            //    {
+            //        countLeft++;
+            //        continue;
+            //    }
+            //    break;
+            //}
+
+            //Array.Reverse(firstTex);
+            //Array.Reverse(secondText);
+
+            //for (int i = 0; i < maxArray.Length; i++)
+            //{
+
+            //    if (firstTex[i] == secondText[i])
+            //    {
+            //        countRight++;
+            //        continue;
+            //    }
+            //    break;
+            //}
+
+            //Console.WriteLine(Math.Max(countLeft, countRight));
+        }
+    }
+}
