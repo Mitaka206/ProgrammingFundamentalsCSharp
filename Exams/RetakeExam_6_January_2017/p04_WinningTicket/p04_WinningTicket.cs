fb767fb93d5da9d636a7c9e1a6a3ddc2510894fc
Mitaka206 <mitaka206@gmail.com>
2017-02-10 17:07:01 +0200

    jhgjkg

--- Exams/RetakeExam_6_January_2017/p04_WinningTicket/p04_WinningTicket.cs
+++ Exams/RetakeExam_6_January_2017/p04_WinningTicket/p04_WinningTicket.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace p04_WinningTicket
 {
@@ -13,39 +11,43 @@ namespace p04_WinningTicket
             string input = Console.ReadLine();
             var ticket = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
 
-            Console.WriteLine(string.Join(" ", ticket));
-            Console.WriteLine();
-            int max = 0;
-            string jack = "10$ Jackpot!";
-
             for (int i = 0; i < ticket.Count; i++)
             {
+
                 if (ticket[i].Contains('$') || ticket[i].Contains('@') || ticket[i].Contains('#') || ticket[i].Contains('^'))
                 {
-                    var winSymbols = ticket[i].ToString().Split(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, StringSplitOptions.RemoveEmptyEntries).OrderByDescending(x => x.Count()).ToList();
-                    string oneSymbol = winSymbols[0].Distinct().ToString();
+                    List<string> winSymbols = ticket[i].ToString().Split(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, StringSplitOptions.RemoveEmptyEntries).OrderByDescending(x => x.Count()).ToList();
+
+                    var oneSymbol = winSymbols[0].First();
+
+                    int max = 0;
 
                     if (winSymbols.Count() > max)
                     {
                         max = winSymbols[0].Count();
-                        
+
                         if (max >= 10)
                         {
-                            Console.WriteLine($"ticket {ticket[i]} - {jack}");
+                            Console.WriteLine("ticket " + $"\"{ticket[i]}\"" + $" - 10{oneSymbol} Jackpot!");
                         }
-                        else if (max >= 6 || max <= 9)
+
+                        if (max >= 6 && max <= 9)
                         {
-                            Console.WriteLine($"ticket {ticket[i]} - {max}{oneSymbol}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid ticket");
+                            Console.WriteLine("ticket " + $"\"{ ticket[i]}\"" + $" - {max}{oneSymbol}");
                         }
                     }
                 }
-                
+                else if (ticket.Contains("validticketnomatch:("))
+                {
+                    Console.WriteLine("ticket " + $"\"{ ticket[i]}\"" + $" - no match");
+                }
+                else
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+
             }
-            
+
         }
     }
 }
