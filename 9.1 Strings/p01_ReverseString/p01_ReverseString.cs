e90c184de20b37ac4e7fa1b45dcfcdc36a9e4012
Mitaka206 <mitaka206@gmail.com>
2017-02-13 11:00:40 +0200

    add from string Lab

---
+++ 9.1 Strings/p01_ReverseString/p01_ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace p01_ReverseString
+{
+    class p01_ReverseString
+    {
+        static void Main(string[] args)
+        { 
+            var input = Console.ReadLine().Reverse().ToList();
+
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
