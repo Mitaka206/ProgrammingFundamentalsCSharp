47aedcb6b6fe020949604f578185c0921ba03a62
Mitaka206 <mitaka206@gmail.com>
2017-02-16 12:05:43 +0200

    test of regex

---
+++ 10.1 Regex/replaceAtag/replaceAtag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace replaceAtag
+{
+    class replaceAtag
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var regex = new Regex(@"<a.*href=""(.*)"">(.*?)<\/a>");
+
+            var result = regex.Replace(input, @"<ul> <li> <a href=""http://softuni.bg"">SoftUni</a></ li > </ ul >");
+
+            Console.WriteLine(result);
+        }
+    }
+}
