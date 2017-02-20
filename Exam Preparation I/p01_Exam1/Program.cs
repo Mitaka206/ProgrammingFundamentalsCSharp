9953d9b0267b94d68e4197c8378d718c9aef8c0e
Mitaka206 <mitaka206@gmail.com>
2017-02-20 10:57:39 +0200

    add p04_WiningTicetOK80

---
+++ Exam Preparation I/p01_Exam1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01_Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
