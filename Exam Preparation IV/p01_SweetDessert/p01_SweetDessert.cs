73c56acf1164327de807923a9babe8c62f9f903a
Mitaka206 <mitaka206@gmail.com>
2017-02-24 07:32:29 +0200

    add Exam Preparation IV

---
+++ Exam Preparation IV/p01_SweetDessert/p01_SweetDessert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01_SweetDessert
+{
+    class p01_SweetDessert
+    {
+        static void Main(string[] args)
+        {
+            var money = double.Parse(Console.ReadLine());
+            var numberOfGuests = double.Parse(Console.ReadLine());
+            var pricesOfBananas = double.Parse(Console.ReadLine());
+            var priceOfEgs = double.Parse(Console.ReadLine());
+            var priceOfBerries = double.Parse(Console.ReadLine());
+
+            var forGuests = Math.Ceiling(numberOfGuests / 6);
+
+            var sum = forGuests*(2 * pricesOfBananas + 4 * priceOfEgs + 0.2 * priceOfBerries);
+
+            if (sum <= money)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {sum:F2}lv.");
+            }
+            else
+            {
+                var odatherSum = sum - money;
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {(sum-money):F2}lv more.");
+            }
+        }
+    }
+}
